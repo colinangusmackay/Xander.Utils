42d05969e8d381325c853ad57a262bf5b0fd3d02
colinangusmackay <colin.mackay@gmail.com>
2014-05-05 20:13:43 +0100

    Add NuGet

---
+++ src/Xander.Utils/Extensions/ObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Xander.Utils.Extensions
+{
+    public static class ObjectExtensions
+    {
+         public static string AsJson(this object target)
+         {
+             
+         }
+    }
+}
